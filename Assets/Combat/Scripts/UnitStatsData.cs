84f742e8fa762d7db1850e4f3861d3d960c8e520
Aton Alwin A. David <atondavid.ad@gmail.com>
2024-09-22 15:19:30 +0800

    feat: Add Dodge Chance and Crit Stats

--- Assets/Combat/Scripts/UnitStatsData.cs
+++ Assets/Combat/Scripts/UnitStatsData.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Unit Stats Data", menuName = "Combat/Unit Stats")]
 public class UnitStatsData : ScriptableObject {
 	public float hitPoints;
-	public float manaPoints;
-	public float speed;
-	public float attack;
-	public float attackSpeed;
 	public float hpRegen;
+	public float manaPoints;
 	public float mpRegen;
 	public float defense;
+	public float attack;
+	public float attackSpeed;
+	public float speed;
+
+	[Range(0f, 1f)] public float dodgeChance;
+	[Range(0f, 1f)] public float critChance;
+	public float critDamage;
 }
